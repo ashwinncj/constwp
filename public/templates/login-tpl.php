0eb06f39c9b11e3df73676eea06ea0cadd09aa04
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-04-22 22:06:12 +0530

    Login template updated styles

--- public/templates/login-tpl.php
+++ public/templates/login-tpl.php
@@ -5,11 +5,11 @@
 <div id="cme-login-form" class="c100 cme-login-form">
     <p id="login-error-message" style="color: red;"></p>
     <label>Email*</label>
-    <input type="email" required id="user-email" name="user_email">
+    <input type="email" required id="user-email" name="user_email" placeholder="Enter your email">
     <label>Password*</label>
-    <input type="password" required id="user-password" name="user_password">
-    <button type="button" id="user-login-button" style="width: 150px; border: none;cursor: pointer;box-shadow: 1px 1px 3px 1px gray">Login</button>
+    <input type="password" required id="user-password" name="user_password" placeholder="Type your password">
+    <button type="button" id="user-login-button" class="cme-login-form-btn">Login</button>
 </div>
 <div id="cme-logout-form" class="c100 cme-login-form" style="display: none;">
-    <button type="button" id="user-logout-button" style="width: 150px; border: none;cursor: pointer;box-shadow: 1px 1px 3px 1px gray">Logout</button>
+    <button type="button" id="user-logout-button"  class="cme-login-form-btn" >Logout</button>
 </div>
