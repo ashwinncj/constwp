a8a293052409c4977290a8aaff2f33b1a8e7a75a
ashwinncj <30401526+ashwinncj@users.noreply.github.com>
2019-04-23 21:14:25 +0530

    Login template updated styles

---
+++ public/templates/register-tpl.php
@@ -0,0 +1,14 @@
+
+<!--//Login Template-->
+<!--Login Methods defined in ConstructMe-public.js-->
+
+<div id="cme-register-form" class="c100 cme-login-form">
+    <p id="register-error-message" style="color: red;"></p>
+    <label>Email*</label>
+    <input type="email" required id="register-user-email" name="user_email" placeholder="Enter your email">
+    <label>Password*</label>
+    <input type="password" required id="register-user-password" name="user_password" placeholder="Type password">
+    <label>Confirm Password*</label>
+    <input type="password" required name="confirm_password" id="register-confirm-password" placeholder="ReType password">
+    <button type="button" id="user-register-button" class="cme-login-form-btn">Register</button>
+</div>
